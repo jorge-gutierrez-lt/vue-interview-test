09105567fe52350a592a050432902085a462c9b8
Jorge Gutiérrez <71669894+jorge-gutierrez-lt@users.noreply.github.com>
2022-06-03 11:06:26 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
-</template>
-
 <script>
-import HelloWorldVue from "./components/HelloWorld.vue";
+import TodoItem from "@/components/TodoItem";
+
 export default {
   name: "App",
-  components: {
-    HelloWorld: HelloWorldVue,
+
+  components: { TodoItem },
+
+  data: () => ({
+    todos: [
+      { id: 1, title: "Do the dishes" },
+      { id: 2, title: "Take out the trash" },
+      { id: 3, title: "Mow the lawn" },
+    ],
+    nextTodoId: 4,
+  }),
+
+  methods: {
+    addNewTodo() {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText,
+      });
+      this.newTodoText = "";
+    },
   },
 };
 </script>
+
+<template>
+  <form v-on:submit.prevent="addNewTodo">
+    <label for="new-todo">Add a todo</label>
+    <input
+      v-model="newTodoText"
+      id="new-todo"
+      placeholder="E.g. Feed the cat"
+    />
+    <button>Add</button>
+  </form>
+  <ul>
+    <todo-item
+      v-for="(todo, index) in todos"
+      :key="todo.id"
+      :title="todo.title"
+      @remove="todos.splice(index, 1)"
+    />
+  </ul>
+</template>
 
 <style>
 #app {
